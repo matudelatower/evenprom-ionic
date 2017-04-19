89c8064b50ea21297232a88e1eb64d54f123e698
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2017-04-19 14:04:00 -0300

    #10 fix loader favoritos

--- src/pages/favoritos/favoritos.ts
+++ src/pages/favoritos/favoritos.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController, NavParams, LoadingController, AlertController} from 'ionic-angular';
 import {MainService} from "../../app/main.service";
 import {ModalPreviewPublicacion} from "../modals/previewPublicacion";
+import {PrincipalPage} from "../principal/principal";
 
 /*
  Generated class for the Favoritos page.
@@ -23,28 +24,42 @@ export class FavoritosPage {
                 public mainService: MainService,
                 public alertCtrl: AlertController) {
 
-        let loader = this.loadingCtrl.create({
-            content: "Cargando Favoritos",
-            // duration: 6000
-        });
-        loader.present();
+
         mainService.getUser().then(
             (userData) => {
+
+                if (userData.nombre=="demo"){
+                    this.navCtrl.pop();
+                    this.mainService.sinUsuario();
+                    return;
+                }
+
+                let loader = this.loadingCtrl.create({
+                    content: "Cargando Favoritos",
+                    // duration: 6000
+                });
+                loader.present();
 
                 this.mainService.getAll('favoritos/' + userData.userID + '/personas')
                     .then(
                         (response) => {
+
                             this.favoritos = response;
                             loader.dismissAll();
 
                         },
                         (err) => {
                             console.log('error timeout');
+
                             loader.dismissAll();
+
+                            this.navCtrl.setRoot(PrincipalPage);
+                            this.mainService.sinUsuario();
                         }
                     );
             },
             error => {
+                this.navCtrl.setRoot(PrincipalPage);
                 this.mainService.sinUsuario();
             }
         )
