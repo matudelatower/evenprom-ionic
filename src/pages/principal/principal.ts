6af211cce6db6fc35392a501e660b8877fa5c5ef
fuguet <fuguet@yahoo.com>
2017-04-30 18:04:22 -0300

    Ajustes en diseño

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,5 +1,5 @@
 import {Component, Output, EventEmitter, ViewChild} from '@angular/core';
-import {ToastController, Slides, Events, Content, Select} from "ionic-angular";
+import {ToastController, Slides, Events, Content, Select, AlertController} from "ionic-angular";
 import {ModalSearch} from '../../pages/modals/search';
 import {MainService} from "../../app/main.service";
 import 'leaflet';
@@ -10,6 +10,7 @@ import {GeosearchComponent} from './../../directives/map/geosearch.component';
 import {ModalMapa} from './modalMapa.component';
 import {Geolocation} from 'ionic-native';
 import {GeocodingService} from "../../directives/map/geocode.service";
+import {TranslateService} from "@ngx-translate/core";
 
 
 @Component({
@@ -41,11 +42,13 @@ export class PrincipalPage {
     search = "";
 
     localidades = [];
+    locals:any='';
 
     user = null;
 
     complete = false;
-
+    oktext:any='Ok';
+    canceltext:any='Cancel';
 
     @ViewChild('searchP') searchP: Select;
 
@@ -53,12 +56,26 @@ export class PrincipalPage {
                 public mainservice: MainService,
                 public loadingCtrl: LoadingController,
                 public toastCtrl: ToastController,
+                public alertCtrl: AlertController,
                 public geoService: GeocodingService,
                 public events: Events,
+                public translateService:TranslateService,
                 public mapService: MapService) {
         this.mapService = mapService;
 
+      translateService.get('ok').subscribe(
+        value => {
+          // value is our translated string
+          this.oktext = value;
+        }
+      );
 
+      translateService.get('cancelar').subscribe(
+        value => {
+          // value is our translated string
+          this.canceltext = value;
+        }
+      );
     }
 
     ngOnInit() {
@@ -119,6 +136,19 @@ export class PrincipalPage {
 
     }
 
+    onSlideChanged() {
+        let currentIndex = this.slider.getActiveIndex();
+        console.log("Current index is", currentIndex);
+        this.tabBody = currentIndex;
+        this.tabs = currentIndex.toString();
+        if (this.tabs == "1") {
+            this.pageEmpresas();
+        }
+        else if (this.tabs == "2") {
+            this.pageNotificaciones();
+        }
+        this.content.scrollToTop();
+    }
 
     selectedTab() {
         // this.slider.slideTo(+this.tabs, 500);
@@ -265,17 +295,82 @@ export class PrincipalPage {
         modal.onDidDismiss((data) => {
 
             if (data) {
+                let fields = "fields=" + JSON.stringify(data);
 
-
-                this.doRefresh(false, {
-                    fields : data
-                });
+                this.doRefresh(false, fields);
             }
 
 
         });
     }
 
+    modalRanking() {
+        let modal = this.mainservice.modalCreate(GeosearchComponent);
+
+        modal.present();
+
+        modal.onDidDismiss((data: any[]) => {
+            if (data) {
+                console.log(data);
+            }
+        });
+    }
+
+    show_alert(){
+      let alert = this.alertCtrl.create();
+      var title='localidad';
+
+      for(var i in this.localidades){
+        alert.addInput({
+          type:'radio',
+          label:this.localidades[i].descripcion,
+          value:this.localidades[i].id,
+          checked:false
+        });
+
+      }
+
+      alert.addButton(this.canceltext);
+      alert.addButton({
+        text: this.oktext,
+        handler: data => {
+          console.log('Checkbox data:', data);
+          this.locals=data;
+        }
+      });
+
+      this.translateService.get(title).subscribe(
+        value => {
+          // value is our translated string
+          alert.setTitle(value);
+
+          alert.present();
+
+        }
+      );
+
+    }
+
+
+    modalEmpresas() {
+        let modal = this.mainservice.modalCreate(Empresas);
+
+        modal.present();
+
+        modal.onDidDismiss((data: any[]) => {
+            if (data) {
+                console.log(data);
+            }
+        });
+    }
+
+    pageEmpresas() {
+        // this.navController.push(Empresas);
+    }
+
+    pageNotificaciones() {
+
+    }
 
     toastPromo(promo) {
 
